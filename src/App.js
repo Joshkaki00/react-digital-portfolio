ac0d4fb3e6b1afe73099045f481b556a468e07d5
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-03-04 15:34:54 -0800

    wrong route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Portfolio from './components/Portfolio';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Portfolio />
+      <Home />
     </div>
   );
 }
